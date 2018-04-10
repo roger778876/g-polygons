d4e67d8496756e26dc385f084d97c7aa82cde4a4
roger778876 <rogercesar778876@gmail.com>
2018-04-10 00:14:46 -0400

    add_box

--- draw.py
+++ draw.py
@@ -36,22 +36,23 @@ def add_box( points, x, y, z, width, height, depth ):
   z1 = z - depth
 
   #front
-  add_edge(points, x, y, z, x1, y, z)
-  add_edge(points, x, y1, z, x1, y1, z)
-  add_edge(points, x1, y, z, x1, y1, z)
-  add_edge(points, x, y, z, x, y1, z)
+  add_polygon(points, x, y, z, x, y1, z, x1, y1, z)
+  add_polygon(points, x1, y, z, x, y, z, x1, y1, z)
+  add_polygon(points, x, y, z1, x, y, z, x1, y, z)
+  add_polygon(points, x1, y, z1, x, y, z1, x1, y, z)
 
   #back
-  add_edge(points, x, y, z1, x1, y, z1)
-  add_edge(points, x, y1, z1, x1, y1, z1)
-  add_edge(points, x1, y, z1, x1, y1, z1)
-  add_edge(points, x, y, z1, x, y1, z1)
+  add_polygon(points, x1, y, z1, x1, y1, z1, x, y1, z1)
+  add_polygon(points, x, y, z1, x1, y, z1, x, y1, z1)
+  add_polygon(points, x, y1, z, x, y1, z1, x1, y1, z1)
+  add_polygon(points, x1, y1, z, x, y1, z, x1, y1, z1)
 
   #sides
-  add_edge(points, x, y, z, x, y, z1)
-  add_edge(points, x1, y, z, x1, y, z1)
-  add_edge(points, x, y1, z, x, y1, z1)
-  add_edge(points, x1, y1, z, x1, y1, z1)
+  add_polygon(points, x1, y, z, x1, y1, z, x1, y1, z1)
+  add_polygon(points, x1, y, z1, x1, y, z, x1, y1, z1)
+  add_polygon(points, x, y, z1, x, y1, z1, x, y1, z)
+  add_polygon(points, x, y, z, x, y, z1, x, y1, z)
+
 
 def add_sphere( edges, cx, cy, cz, r, step ):
   points = generate_sphere(cx, cy, cz, r, step)
